4bc53c8e807f5f8caf1be661972024c911a5a797
Dan Lively <daniel.lively@plusconsulting.com>
2013-08-07 10:27:27 -0400

    updated manifest version

--- manifest.php
+++ manifest.php
@@ -4,7 +4,7 @@
 $manifest = array(
     'acceptable_sugar_flavors' => array('CE', 'PRO', 'CORP', 'ENT', 'ULT'),
     'acceptable_sugar_versions' => array(
-
+		'regex_matches' => array ('6\\.5\\.(.*?)', '6\\.6\\.(.*?)', '6\\.6\\.(.*?)\\.(.*?)', '6\\.7\\.(.*?)', '6\\.7\\.(.*?)\\.(.*?)'),
     ),
     'readme' => '',
     'author' => 'Dan Lively',
